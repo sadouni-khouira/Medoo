b6738a7beed9a3dab230c34412899d3718c49704
sadouni-khouira <67142763+sadouni-khouira@users.noreply.github.com>
2020-11-26 08:23:47 +0100

    Delete test_scraper.php

--- C - Niveau - III - Boss Rush Backend/[Boss] Un scraper d'emails/test_scraper.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'email_scraper.php' ;
-$url = 'https://github.com/nyxgeek/username-lists/blob/master/usernames-top100/usernames_gmail.com.txt';
-$emails = scrape_email($url);
-echo implode($emails, '<br>');
-?>
